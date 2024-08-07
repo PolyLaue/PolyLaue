c4304f968a561ce05bdd6edcae8d19ac65a46795
Patrick Avery <patrick.avery@kitware.com>
2024-08-07 11:30:56 -0500

    Do not hold file handle to external reflections
    Users want to modify the HDF5 file while the application is running,
    and on Windows, they cannot do this if the file is held open. Only
    open the file when we want to read something.
    
    Maybe this makes it run slower, but it's not that noticeable, and
    it allows users to edit the file.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- polylaue/model/reflections/external.py
+++ polylaue/model/reflections/external.py
@@ -7,22 +7,30 @@ from polylaue.model.reflections.base import BaseReflections
 
 class ExternalReflections(BaseReflections):
     def __init__(self, filepath: PathLike):
+        # We currently do not keep the HDF5 file open by holding onto
+        # a file handle, because users want to be able to edit this
+        # file while the application is running, and they want to see
+        # the updates immediately. On Windows (the primary OS we are
+        # supporting), if any application has an open file handle on
+        # the file, we cannot edit it. So, for now, only open the
+        # file when we are actually reading something from it.
         self.filepath = filepath
-        self.h5_file = h5py.File(filepath, 'r')
 
     def reflections_table(
         self, row: int, column: int, scan_number: int
     ) -> np.ndarray | None:
         path = self._reflections_table_path(row, column, scan_number)
 
-        if path not in self.h5_file:
-            return None
+        with h5py.File(self.filepath, 'r') as f:
+            if path not in f:
+                return None
 
-        return self.h5_file[path][()]
+            return f[path][()]
 
     @property
     def num_crystals(self):
-        return len(self.h5_file['/crystals'])
+        with h5py.File(self.filepath, 'r') as f:
+            return len(f['/crystals'])
 
     def _reflections_table_path(
         self, row: int, column: int, scan_number: int
