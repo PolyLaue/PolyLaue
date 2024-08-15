8df8b0c2cdb21c1580ff7fae4a0596b54729275f
Patrick Avery <patrick.avery@kitware.com>
2024-08-15 08:18:34 -0500

    Add script for adding copyright comments
    There is a copyright comment that must be included at the top of every
    source file. This script automatically adds that comment if it is missing.
    
    We will also use this script with CI to ensure no files are added without
    the copyright comment.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ scripts/add_copyright_to_source_files.py
@@ -0,0 +1,33 @@
+# Copyright © 2024, UChicago Argonne, LLC. See "LICENSE" for full details.
+
+"""Utility script to add copyright notice to all source files.
+
+This project has a requirement to include the copyright comment at the top
+of every source file. This utility script helps apply that automatically.
+
+This script is intended to be ran from the root level of the repository.
+"""
+
+from pathlib import Path
+
+copyright_text = '# Copyright © 2024, UChicago Argonne, LLC. See "LICENSE" for full details.\n'  # noqa
+
+num_files_edited = 0
+for filename in Path('.').rglob('*.py'):
+    with open(filename, 'r') as rf:
+        first_line = rf.readline()
+
+    if first_line != copyright_text:
+        print('Adding copyright text to:', filename)
+        with open(filename, 'r') as rf:
+            contents = rf.read()
+
+        # Only include the carriage return if there are file contents
+        endline = '\n' if contents else ''
+        contents = copyright_text + endline + contents
+        with open(filename, 'w') as wf:
+            wf.write(contents)
+
+        num_files_edited += 1
+
+print('Number of files edited:', num_files_edited)
