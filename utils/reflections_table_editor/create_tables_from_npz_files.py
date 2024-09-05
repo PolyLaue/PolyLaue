819ef721492c272753073d9f93868d061b4c603c
Patrick Avery <patrick.avery@kitware.com>
2024-09-05 11:08:37 -0500

    Fix `create_tables_from_npz_files.py` script
    This script now correctly stacks tables for each crystal together.
    
    The user is also able to manually set and edit the crystal_id for
    each npz file.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- utils/reflections_table_editor/create_tables_from_npz_files.py
+++ utils/reflections_table_editor/create_tables_from_npz_files.py
@@ -13,17 +13,29 @@ scan_pos_x = 1
 scan_pos_y = 1
 
 # Each NPZ file is for a separate crystal ID
-npz_files = [
-    'predicted_list.npz',
+# The key is the crystal ID. The value is the file name.
+npz_files = {
+    0: 'predicted_list.npz',
+    1: 'predicted_list0.npz',
     # Add more, one for each crystal
-]
+}
 
-# NOTE: at this point, you need to call `editor.set_crystals_table()`
-# with the crystals in the same order as the order of the NPZ files above.
+# NOTE: if the crystals_table doesn't already exist on the HDF5 file,
+# you will need to also set it, something simlar to the following:
 
-# editor.set_crystals_table(crystals_table)
+crystals_table = np.array(
+    [
+        # Each row of the crystal table is just the 9 parameters
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ]
+)
 
-for crystal_id, filename in enumerate(npz_files):
+editor.set_crystals_table(crystals_table)
+
+# Stack all tables together in one list
+all_tables = []
+for crystal_id, filename in npz_files.items():
     f = np.load(filename)
     pred_list1 = f['ipred_list1']
     pred_list2 = f['ipred_list2']
@@ -47,10 +59,12 @@ for crystal_id, filename in enumerate(npz_files):
     # Add the crystal ID into the 9th column
     table = np.insert(table, 9, crystal_id, axis=1)
 
-    # Now write this to the HDF5 file
-    editor.set_reflections_table(
-        table,
-        scan_num,
-        scan_pos_x,
-        scan_pos_y,
-    )
+    all_tables.append(table)
+
+# Now write all tables to the HDF5 file
+editor.set_reflections_table(
+    np.vstack(all_tables),
+    scan_num,
+    scan_pos_x,
+    scan_pos_y,
+)
