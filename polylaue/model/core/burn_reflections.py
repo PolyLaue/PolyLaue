7fae8af895a2482ba4c01324fa6b31285df761ca
Patrick Avery <patrick.avery@kitware.com>
2025-02-28 09:03:12 -0600

    Add 'fcc' structure type for burning reflections
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- polylaue/model/core/burn_reflections.py
+++ polylaue/model/core/burn_reflections.py
@@ -7,6 +7,7 @@ VALID_STRUCTURE_TYPES = [
     '',
     'hcp',
     'Diamond',
+    'fcc',
 ]
 
 
@@ -302,6 +303,10 @@ def burn(
             (i + 1), round((float(sta_mul[i]) * 100.0 / sta_pre_tot), 2), '%'
         )
     if structure_type != '':
+        if structure_type == 'fcc':
+            ref_cond1 = range(0, (max_h + max_k + 1), 2)
+            ref_cond2 = range(0, (max_h + max_l + 1), 2)
+            ref_cond3 = range(0, (max_l + max_k + 1), 2)
         if structure_type == 'hcp':
             ref_cond1 = range(0, (max_l + 1), 2)
             ref_cond2 = range(1, (max_h + max_k + 1), 3)
@@ -323,6 +328,19 @@ def burn(
                 h = int(aaa[0]) * i
                 k = int(aaa[1]) * i
                 l = int(aaa[2]) * i  # noqa
+                if structure_type == 'fcc':
+                    ch_ch = -3
+                    if abs(h + k) in ref_cond1:
+                        ch_ch = ch_ch + 1
+                    if abs(h + l) in ref_cond2:
+                        ch_ch = ch_ch + 1
+                    if abs(l + k) in ref_cond3:
+                        ch_ch = ch_ch + 1
+                    if ch_ch == 0:
+                        if ch_ch_ch == 1:
+                            ch_ch_ch = 0
+                            nnn1 = i
+                        nnn2 = i
                 if structure_type == 'hcp':
                     ch_ch = -1
                     if abs(l) in ref_cond1:
