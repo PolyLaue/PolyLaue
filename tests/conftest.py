4d87327a18b9179d3eeeee73617a447ace8f2324
Patrick Avery <patrick.avery@kitware.com>
2025-07-14 15:49:35 -0500

    Add missing changes to conftest.py for tests
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import json
 import os
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 TEST_DIR = Path(__file__).resolve().parent
@@ -23,3 +24,18 @@ def test_project_manager_serialized():
     path = TEST_DIR / 'test_project_manager.json'
     with open(path, 'r') as rf:
         return json.load(rf)
+
+
+@pytest.fixture
+def test_dir() -> Path:
+    return TEST_DIR
+
+
+@pytest.fixture
+def test_geometry(test_dir: Path) -> dict[str, np.ndarray]:
+    npz_file = np.load(test_dir / 'data/geometry.npz')
+    return {
+        'det_org': npz_file['iitt1'],
+        'beam_dir': npz_file['iitt2'],
+        'pix_dist': npz_file['iitt3'],
+    }
